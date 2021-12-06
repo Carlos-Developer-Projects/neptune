4f2a00edc8755c54baf95e4b0f74c49d7a5ff271
Carlos Aviles Buitron <admin@carlos-developer.com>
2021-12-06 18:09:47 -0500

    Comment out redux until ready

--- 01-frontend/pages/_app.js
+++ 01-frontend/pages/_app.js
@@ -1,21 +1,22 @@
 import 'tailwindcss/tailwind.css';
 import "../styles/index.scss";
-import { Provider } from 'react-redux';
 import { MoralisProvider } from "react-moralis";
-import { createWrapper } from 'next-redux-wrapper';
-import store from '../redux/store';
+//redux
+// import { createWrapper } from 'next-redux-wrapper';
+// import { Provider } from 'react-redux';
+// import store from '../redux/store';
  
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider store={store}>
       <MoralisProvider appId={process.env.NEXT_PUBLIC_APP} serverUrl={process.env.NEXT_PUBLIC_URL}>
             <Component {...pageProps} />
       </MoralisProvider>
-    </Provider>
   );
 }
 
-const makeStore = ()=>store;
-const wrapper = createWrapper(makeStore);
+export default MyApp;
 
-export default wrapper.withRedux(MyApp);   
+// const makeStore = ()=>store;
+// const wrapper = createWrapper(makeStore);
+
+// export default wrapper.withRedux(MyApp);   
