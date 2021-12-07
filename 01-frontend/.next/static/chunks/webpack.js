3137729da506f725eb6308967327b7c24f3b4293
Carlos Aviles Buitron <admin@carlos-developer.com>
2021-12-07 16:41:14 -0500

    Add section header component

--- 01-frontend/.next/static/chunks/webpack.js
+++ 01-frontend/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f776647209c66b3"; }
+/******/ 		__webpack_require__.h = function() { return "787ca1516baa8258"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ 01-frontend/src/components/dashboard/sectionHeader.js
@@ -0,0 +1,7 @@
+export default function SectionHeader({title}){
+    return(
+        <div className="pb-5 border-b border-gray-200 mb-4">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">{title}</h3>
+        </div>
+    )
+}
