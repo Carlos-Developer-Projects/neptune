d0d67fa6ba83ae41bdd719c4e7ed33b7b4d92a96
Carlos-Developer-Projects <projects@carlos-developer.com>
2021-12-15 22:44:51 +0000

    Configure frontend port

--- 01-frontend/.next/static/chunks/webpack.js
+++ 01-frontend/.next/static/chunks/webpack.js
@@ -136,7 +136,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b0dcec4b124d3cbb"; }
+/******/ 		__webpack_require__.h = function() { return "35894c01470a7519"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
